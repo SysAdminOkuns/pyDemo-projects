43ed146c491a2838cea4119cd5d4823a91dc6d68
Nosa <sysad.okundaye@gmail.com>
2023-10-23 17:07:17 +0000

    added a new line

--- hello.py
+++ hello.py
@@ -2,5 +2,6 @@
 #changes made by sreedevi
 # Another comment from cloud9 IDE
 # Yet another comment from cloud9 Integrated development environment
+print("Welcome to AWSome program!")
 userInput = input("Please, enter your name: ")
 print("Hello", userInput)
